b37c1040b8c059c180bd06ef7555310cacfeca39
isabelyb <isabelcristinayepesb@gmail.com>
2021-10-18 13:53:45 -0500

    name main to app

---
+++ app/start.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+from mongo_db import mongodb, accumulate, save_input, tables
+
+def create_app():
+    app = Flask(__name__)
+    return app
+
+
+app = create_app()
+db = mongodb()
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def data():
+    return save_input()
+
+
+@app.route('/submit')
+def submit():    
+    context = {
+        'variable':'Mariu',
+        'titles': ['NUMBERS','TEXT','CHARACTER'],
+        'tables': tables()
+    }    
+    return render_template('submit.html', **context)
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
